4423620cce94dc8c97047c15c6b9bd6d121ae29d
santiomodey <54156698+santiomodey@users.noreply.github.com>
2020-09-26 16:14:20 -0300

    Singleton

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Library
+{
+    public class Singleton<T>
+        where T : new()
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+    }       
+}
